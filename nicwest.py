4d9d08230af91c72f77db86ceb928ed12694c1f0
Nic West <nicwest.cb@gmail.com>
2014-02-05 19:57:42 +0000

    house cleaning

--- nicwest.py
+++ nicwest.py
@@ -15,9 +15,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/history')
-def history():
-    return render_template('history.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
