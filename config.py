3aed9422e03e8984783ae69c7d3f61fa0e701315
Nic West <nicwest.cb@gmail.com>
2014-02-10 04:07:20 +0000

    secrets

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 
 CSRF_ENABLED = True
-SECRET_KEY = 'WHYSHOULDITELLYOU?'
+SECRET_KEY = 'VqpjqpF3Cge3EGz1cpb7'
