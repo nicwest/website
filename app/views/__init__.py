6a1753d5228cefdd2d59dac09761d6aba48df681
Nic West <nicwest.cb@gmail.com>
2014-02-10 03:20:35 +0000

    updates

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,2 +1,3 @@
+from app.views.main import *
 from app.views.cv import *
 from app.views.admin import *
--- app/views/cv.py
+++ app/views/cv.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template
 from app import app, db, models
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/cv')
 def cv_display():
-    return render_template('cv_template.html')
+    return render_template('temp_cv.html')
 
