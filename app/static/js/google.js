c56baeb21381bdc124acc21defdfbcb3c893762e
Nic West <nicwest.cb@gmail.com>
2014-02-10 04:04:20 +0000

    analytics

--- app/static/js/google.js
+++ app/static/js/google.js
@@ -1,1 +1,13 @@
 // PASTE GOOGLE CODE HERE FOR EXAMPLE ANALYTICS
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-47906997-1']);
+  _gaq.push(['_setDomainName', 'nic-west.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
