958f32344df97fe1aeb57aefea59c3308ce2a1c1
Nic <nic@stunjelly.com>
2014-02-21 18:05:16 +0000

    blog work

--- app/forms/admin.py
+++ app/forms/admin.py
@@ -11,8 +11,11 @@ class NewCV(Form):
 
 
 class Item(Form):
-    parent = SelectField('parent', choices=[('None (Category)', None)]+db.session.query(models.Item.id, models.Item.admin_title).all())
-    typ = SelectField('server', choices=db.session.query(models.Item.typ, models.Item.typ).group_by(models.Item.typ).all())
+    try:
+        parent = SelectField('parent', choices=[('None (Category)', None)]+db.session.query(models.Item.id, models.Item.admin_title).all())
+        typ = SelectField('server', choices=db.session.query(models.Item.typ, models.Item.typ).group_by(models.Item.typ).all())
+    except:
+        pass
     key = TextField('title', validators=[Required()])
     value = TextAreaField('overview', validators=[Required()])
     admin_title = TextField('title', validators=[Required()])
--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
-from app import app
+from app import app, models
 
 
 @app.route('/')
 def index():
+    posts = models.Post.query.order_by(models.Post.date.desc()).all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/blog/<slug>')
+def view_post(slug):
     return render_template('index.html')
 
+@app.route('/blog/tag/<slug>')
+def view_tag(slug):
+    return render_template('blog.html')
